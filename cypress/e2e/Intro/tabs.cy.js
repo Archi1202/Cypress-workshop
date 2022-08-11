bdaa906c74e3778547d84e75f46bb32c0b3a380a
Anuar Zhangeldi <anuar1202@gmail.com>
2022-08-11 22:12:05 +0600

    add: locators and assertions tests

--- cypress/e2e/Intro/tabs.cy.js
+++ cypress/e2e/Intro/tabs.cy.js
@@ -1,27 +1,24 @@
 /// <reference types="Cypress" />
 
-describe('Locators: Get elements from Tabs section'),
-  () => {
-    beforeEach('Navigate to test tabs section', () => {
-      cy.visit('https://demoqa.com/tabs');
+describe('Locators: Get elements from Tabs section', () => {
+  beforeEach('Navigate to test tabs section', () => {
+    cy.visit('https://demoqa.com/tabs');
+  });
+  it('Check tabs', () => {
+    // By id
+    cy.get('#demo-tab-what').should('have.class', 'nav-item nav-link active');
+
+    // By tag name
+    // This is not a html tag name, it does not return anything
+    // changed to the tag name nav
+    cy.get('nav').should('have.class', 'nav nav-tabs');
+
+    // By attribute name
+    // This is not attribute name
+    // changed to the attribute name
+    cy.get('#demo-tab-what').then((element) => {
+      expect(element).to.have.text('What');
+      expect(element).to.have.class('nav-item nav-link active');
     });
-    it('Check tabs', () => {
-      // By id
-      cy.get('#demo-tab-what');
-
-      // By tag name
-      // This is not a html tag name, it does not return anything
-      // changed to the tag name nav
-      cy.get('nav');
-
-      // By attribute name
-      // This is not attribute name
-      // changed to the attribute name
-      cy.get('aria-controls');
-
-      // By class value value
-      // This is not attribute name and value. You also have only one closed bracket, you haven't opened it anytwhere
-      // Corrected
-      cy.get('[class="nav-item nav-link disabled"]');
-    });
-  };
+  });
+});
